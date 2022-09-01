fba0675c94fd6fdfa01743cc22a8425f5b318fa9
Timo Tännler <timo.taennler@protonmail.com>
2022-09-01 23:56:18 +0300

    added name to work project

--- components/work/workThumbnail.tsx
+++ components/work/workThumbnail.tsx
@@ -20,19 +20,26 @@ export const WorkThumbnail: FC<WorkThumbnailProps> = ({
       className={`work-thumbnail-${num} work-thumbnail relative `}
       style={{ paddingTop }}
     >
-      <Link href={`/${name}`}>
+      <Link href={`/work/${name}`}>
         <a>
-          <Image
-            alt="Portraitbild"
-            src="/images/placeholder2.jpg"
-            layout="fill"
-            objectFit="contain"
-            onLoad={({ target }) => {
-              const { naturalWidth, naturalHeight } =
-                target as HTMLImageElement;
-              setPaddingTop(`calc(100% / (${naturalWidth} / ${naturalHeight})`);
-            }}
-          />
+          <div>
+            <Image
+              alt="Portraitbild"
+              src="/images/placeholder2.jpg"
+              layout="fill"
+              objectFit="contain"
+              onLoad={({ target }) => {
+                const { naturalWidth, naturalHeight } =
+                  target as HTMLImageElement;
+                setPaddingTop(
+                  `calc(100% / (${naturalWidth} / ${naturalHeight})`
+                );
+              }}
+            />
+          </div>
+          <div className="work-name-wrapper absolute bottom-0 inset-0 flex justify-center items-center">
+            <div className="work-name text-3xl font-bold">{name}</div>
+          </div>
         </a>
       </Link>
     </div>
