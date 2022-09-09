11c8053073373b9e850626caf97115e6973b66d7
Timo Tännler <timo.taennler@protonmail.com>
2022-09-09 11:05:59 +0300

    added more projects to grid

--- components/work/workThumbnail.tsx
+++ components/work/workThumbnail.tsx
@@ -21,17 +21,19 @@ export const WorkThumbnail: FC<WorkThumbnailProps> = ({
   return (
     <div
       className={`work-thumbnail-${num} work-thumbnail md:hover:scale-105 md:hover:z-10 relative overflow-hidden rounded shadow hover:shadow-md mb-4 md:mb-0 `}
-      style={{order: numMobile}}
+      style={{ order: numMobile }}
     >
       <Link href={`/work/${slug}`}>
         <a>
           <div className="thumbnail-wrapper">
-            <Image
-              alt="Portraitbild"
-              src={urlForImage(imageUrl).url()}
-              layout="fill"
-              objectFit="cover"
-            />
+            {imageUrl && (
+              <Image
+                alt="Portraitbild"
+                src={urlForImage(imageUrl).url()}
+                layout="fill"
+                objectFit="cover"
+              />
+            )}
           </div>
           <div className="work-name-wrapper opacity-100 md:opacity-0 md:hover:opacity-100 absolute bottom-0 inset-0 flex justify-center items-center">
             <div className="work-name text-3xl font-bold">{name}</div>
