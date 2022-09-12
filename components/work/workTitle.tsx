3111a1f90e2eb4aa670898f499830607ab016278
Timo Tännler <timo.taennler@protonmail.com>
2022-09-13 00:23:38 +0300

    mobile fixes

--- components/work/workTitle.tsx
+++ components/work/workTitle.tsx
@@ -8,8 +8,8 @@ interface props {
 
 export const WorkTitle: FC<props> = ({ title, subtitle, description }) => (
   <div className="mb-4">
-    <h1 className="text-4xl md:text-6xl font-bold">
-      {title} {subtitle && <span className="font-thin">{subtitle}</span>}
+    <h1 className="text-4xl md:text-6xl font-bold flex flex-col md:flex-row">
+      {title} {subtitle && <span className="font-thin md:ml-3">{subtitle}</span>}
     </h1>
     <p className="md:text-lg font-thin mt-2">{description}</p>
   </div>
