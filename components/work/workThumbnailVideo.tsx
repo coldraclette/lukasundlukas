b91d1aa95215cffa112dcfc258b702e508356919
Timo Tännler <timo.taennler@protonmail.com>
2022-09-13 00:15:49 +0300

    mobile fix

--- components/work/workThumbnailVideo.tsx
+++ components/work/workThumbnailVideo.tsx
@@ -11,6 +11,8 @@ export const WorkThumbnailVideo: FC<props> = ({ videoWebm, videoMp4 }) => {
     <div className="h-full w-full">
       <video
         preload="auto"
+        muted
+        playsInline
         autoPlay
         loop
         className="object-cover h-full w-full"
